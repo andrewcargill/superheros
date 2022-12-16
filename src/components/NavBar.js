1c0d387ad28d07f4d6a4350bf8a9f76d14ccc721
andrewcargill <andycargill01@aol.com>
2022-12-16 14:44:35 +0000

    format basic navigation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,30 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
+import { NavLink } from "react-router-dom";
+import styles from "../styles/NavBar.module.css"
 
 const NavBar = () => {
   return (
     <Navbar expand="md" fixed="top">
       <Container>
+        <NavLink to="/">
         <Navbar.Brand>
           <img src={logo} alt="logo" height="45" />
         </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link>
-              <i className="fas fa-home"></i>Home
-            </Nav.Link>
-            <Nav.Link>
-              <i className="fas fa-sign-in-alt"></i>Sign in
-            </Nav.Link>
-            <Nav.Link>
-              <i className="fas fa-user-plus"></i>Sign up
-            </Nav.Link>
+            <NavLink to="/" className={styles.nav}>
+              Home
+            </NavLink>
+            <NavLink to="signin" className={styles.nav}>
+              Sign in
+            </NavLink>
+            <NavLink to="signup" className={styles.nav}>
+              Sign up
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
