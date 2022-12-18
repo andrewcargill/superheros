6baeafe7a30221b467d9fb1235539b01a0d3a48b
andrewcargill <andycargill01@aol.com>
2022-12-18 17:55:18 +0000

    add key prop to posts list view

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -4,54 +4,60 @@ import axios from "axios";
 import { Container, Row, Media, Card } from "react-bootstrap";
 import Avatar from "../../components/Avatar";
 import { Link } from "react-router-dom";
+import Asset from "../../components/Asset";
+
 
 function PostsPage() {
   const [posts, setPosts] = useState([]);
 
+
   useEffect(() => {
     async function fetchPosts() {
-       const URL = `/posts/`
-       try {
-          const res = await axios.get(URL)
-          console.log(res.data)
-          setPosts(res.data)
-       } catch (error) {
-          console.log(error)
-       }
+      const URL = `/posts/`;
+      try {
+        const res = await axios.get(URL);
+        console.log(res.data);
+        setPosts(res.data);
+      } catch (error) {
+        console.log(error);
+      }
     }
-    fetchPosts()
- }, [])
+  
+    fetchPosts();
+  }, []);
 
- if (!posts.length) return <h3>Loading...</h3>
+  if (!posts.length) return <h3>Loading...</h3>;
 
   return (
     <div>
-        <div>
-      {posts.map((post) => (
-        <Container>
-        <Row>
-            <Media className="align-items-center justify-content-between">
-              <Link to={`/profiles/${post.profile_id}`}>
-                <Avatar src={post.profile_image} height={55} />
-                {post.owner}
-              </Link>
-            </Media>
-          </Row>
-          <Row>
-            <Link to={`/posts/${post.id}`}>
-              <Card.Img src={post.image} alt={post.caption} height={400} />
-            </Link>
-          </Row>
-          <div>{post.caption}</div>
-          {post.updated_at}
+      <div>
+        {posts.map((post) => (
+          <Container key={post.id}>
 
         
-        </Container>
+            <Row>
+              <Media className="align-items-center justify-content-between">
+                <Link to={`/profiles/${post.profile_id}`}>
+                  <Avatar src={post.profile_image} height={55} />
+                  {post.owner}
+                </Link>
+              </Media>
+            </Row>
+            <Row>
+              <Link to={`/posts/${post.id}`}>
+                <Card.Img src={post.image} alt={post.caption} height={400} />
+              </Link>
+            </Row>
+            <div>{post.caption}</div>
+            {post.updated_at}
 
-      ))}
-    </div>
-    </div>
+          
 
+
+          </Container>
+        ))}
+      </div>
+    </div>
   );
 }
 
