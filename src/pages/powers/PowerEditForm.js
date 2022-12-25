f790e57c37464d50d0677c6ae25e7df493f9b075
andrewcargill <andycargill01@aol.com>
2022-12-25 07:55:24 +0000

    Flesh out powers edit form

--- src/pages/powers/PowerEditForm.js
+++ src/pages/powers/PowerEditForm.js
@@ -3,14 +3,15 @@ import React, { useEffect, useRef, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Alert from "react-bootstrap/Alert";
 import btnStyles from "../../styles/Button.module.css";
-
+import frame from "../../styles/Containers.module.css";
 import { useHistory } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
-
+import appStyle from "../../App.module.css";
 function PowerEditForm() {
   const [errors, setErrors] = useState({});
 
@@ -26,22 +27,33 @@ function PowerEditForm() {
   });
   const { speed, flight, strength, vision, fire, lasers } = powerData;
   const history = useHistory();
-  const {id} = useParams();
+  const { id } = useParams();
 
   useEffect(() => {
     const handleMount = async () => {
-        try{
-            const {data} = await axiosReq.get(`/powers/${id}/`)
-            const {owner, is_owner, speed, flight, strength, vision, fire, lasers } = data;
-            console.log({data});
-            is_owner ? setPowerData({speed, flight, strength, vision, fire, lasers }) : history.push('/')
-        }catch(err){
-            console.log(err);
-        }
+      try {
+        const { data } = await axiosReq.get(`/powers/${id}/`);
+        const {
+          owner,
+          is_owner,
+          speed,
+          flight,
+          strength,
+          vision,
+          fire,
+          lasers,
+        } = data;
+        console.log({ data });
+        is_owner
+          ? setPowerData({ speed, flight, strength, vision, fire, lasers })
+          : history.push("/");
+      } catch (err) {
+        console.log(err);
+      }
     };
 
     handleMount();
-  },[history, id]);
+  }, [history, id]);
 
   const handleChange = (event) => {
     setPowerData({
@@ -59,7 +71,7 @@ function PowerEditForm() {
     formData.append("vision", vision);
     formData.append("fire", fire);
     formData.append("lasers", lasers);
-    
+
     try {
       await axiosReq.put(`/powers/${id}`, formData);
       history.push(`/profiles/${id}`);
@@ -72,125 +84,133 @@ function PowerEditForm() {
   };
 
   const textFields = (
-
-
     <div className="text-center">
-    <Form.Group>
-      <Form.Label>speed</Form.Label>
-      <Form.Control
-        type="number"
-        name="speed"
-        max="5"
-        value={speed}
-        onChange={handleChange}
-      />
-    </Form.Group>
-    {errors?.speed?.map((message, idx) => (
-      <Alert variant="warning" key={idx}>
-        {message}
-      </Alert>
-    ))}
-    
-    <Form.Group>
-      <Form.Label>flight</Form.Label>
-      <Form.Control
-        type="number"
-        name="flight"
-        max="5"
-        value={flight}
-        onChange={handleChange}
-      />
-    </Form.Group>
-    {errors?.flight?.map((message, idx) => (
-      <Alert variant="warning" key={idx}>
-        {message}
-      </Alert>
-    ))}
-    
-    <Form.Group>
-      <Form.Label>strength</Form.Label>
-      <Form.Control
-        type="number"
-        name="strength"
-        max="5"
-        value={strength}
-        onChange={handleChange}
-      />
-    </Form.Group>
-    {errors?.strength?.map((message, idx) => (
-      <Alert variant="warning" key={idx}>
-        {message}
-      </Alert>
-    ))}
-    
-    <Form.Group>
-      <Form.Label>vision</Form.Label>
-      <Form.Control
-        type="number"
-        name="vision"
-        max="5"
-        value={vision}
-        onChange={handleChange}
-      />
-    </Form.Group>
-    {errors?.vision?.map((message, idx) => (
-      <Alert variant="warning" key={idx}>
-        {message}
-      </Alert>
-    ))}
-    
-    <Form.Group>
-      <Form.Label>fire</Form.Label>
-      <Form.Control
-        type="number"
-        name="fire"
-        max="5"
-        value={fire}
-        onChange={handleChange}
-      />
-    </Form.Group>
-    {errors?.fire?.map((message, idx) => (
-      <Alert variant="warning" key={idx}>
-        {message}
-      </Alert>
-    ))}
-    
-    <Form.Group>
-      <Form.Label>lasers</Form.Label>
-      <Form.Control
-        type="number"
-        name="lasers"
-        max="5"
-        value={lasers}
-        onChange={handleChange}
-      />
-    </Form.Group>
-    {errors?.lasers?.map((message, idx) => (
-      <Alert variant="warning" key={idx}>
-        {message}
-      </Alert>
-    ))}
+      <Row className="align-contents-center">
+        <Col>
+          <Form.Group>
+            <Form.Label>speed</Form.Label>
+            <Form.Control
+              type="number"
+              name="speed"
+              max="5"
+              value={speed}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          {errors?.speed?.map((message, idx) => (
+            <Alert variant="warning" key={idx}>
+              {message}
+            </Alert>
+          ))}
 
-    <Button
-      className={`${btnStyles.Button} ${btnStyles.Blue}`}
-      onClick={() => history.goBack()}
-    >
-      cancel
-    </Button>
-    <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
-      update
-    </Button>
-  </div>
+          <Form.Group>
+            <Form.Label>flight</Form.Label>
+            <Form.Control
+              type="number"
+              name="flight"
+              max="5"
+              value={flight}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          {errors?.flight?.map((message, idx) => (
+            <Alert variant="warning" key={idx}>
+              {message}
+            </Alert>
+          ))}
+
+          <Form.Group>
+            <Form.Label>strength</Form.Label>
+            <Form.Control
+              type="number"
+              name="strength"
+              max="5"
+              value={strength}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          {errors?.strength?.map((message, idx) => (
+            <Alert variant="warning" key={idx}>
+              {message}
+            </Alert>
+          ))}
+        </Col>
+        <Col>
+          <Form.Group>
+            <Form.Label>vision</Form.Label>
+            <Form.Control
+              type="number"
+              name="vision"
+              max="5"
+              value={vision}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          {errors?.vision?.map((message, idx) => (
+            <Alert variant="warning" key={idx}>
+              {message}
+            </Alert>
+          ))}
+
+          <Form.Group>
+            <Form.Label>fire</Form.Label>
+            <Form.Control
+              type="number"
+              name="fire"
+              max="5"
+              value={fire}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          {errors?.fire?.map((message, idx) => (
+            <Alert variant="warning" key={idx}>
+              {message}
+            </Alert>
+          ))}
+
+          <Form.Group>
+            <Form.Label>lasers</Form.Label>
+            <Form.Control
+              type="number"
+              name="lasers"
+              max="5"
+              value={lasers}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          {errors?.lasers?.map((message, idx) => (
+            <Alert variant="warning" key={idx}>
+              {message}
+            </Alert>
+          ))}
+        </Col>
+      </Row>
+
+      <Button
+        className={`${btnStyles.ButtonWhite} ${appStyle.Terminal} btn text-uppercase`}
+        onClick={() => history.goBack()}
+      >
+        cancel
+      </Button>
+      <Button
+        className={`${btnStyles.ButtonWhite} ${appStyle.Terminal} btn text-uppercase`}
+        type="submit"
+      >
+        update
+      </Button>
+    </div>
   );
 
   return (
     <Form onSubmit={handleSubmit}>
-      <Row>
-        <Container>
-      {textFields}
+      <div>
+        <Container
+          className={`${frame.ContainerBlackBorder} ${appStyle.Terminal} container-fluid`}
+        >
+          <p> Please update with your official ratings - </p>
+          {textFields}
         </Container>
-
-      </Row>
+      </div>
     </Form>
   );
 }
